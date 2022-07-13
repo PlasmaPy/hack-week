0bf52ad9a7b0edc71bf1da0b5cdb51946dce0cd4
gh012eed <Ghzhouyi@gmail.com>
2022-07-13 14:24:50 -0700

    Added gyroradius function

---
+++ hack/Length/gyroradius.py
@@ -0,0 +1,10 @@
+import astropy.units as u
+import numpy as np
+from astropy.constants.si import e
+
+def gyroradius(
+    mass: u.kg,
+    B: u.T,
+    Vperp: u.m / u.s = np.nan * u.m / u.s,
+) -> u.m:
+    return mass*Vperp/(B*e)
