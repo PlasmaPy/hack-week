4d30d727a39f3b09aec5d3f94fc99177d1f7e5a3
Xuan Sun <xuan.sun1@gmail.com>
2022-07-13 11:34:44 -0700

    add initial alfven speed function
    simple function for calculating the alfven speed

---
+++ hack/alfven_speed.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 11:17:51 2022
+
+@author: xuans
+"""
+import astropy.units as u
+from astropy.constants.si import mu0
+import numpy as np
+
+def Alfven_speed(B: u.T, density: (u.kg / u.m**3)) -> u.m/u.s:
+    
+    return np.abs(B) / np.sqrt(mu0 / density)
+
