ad539eea6e59c3aa7093590f8f6f1dc770890e1a
AshganTaleb <ashgan.tawfik@gmail.com>
2022-07-13 14:35:10 -0400

    add inital alfven speed function

---
+++ hack/alfven_speed.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 14:17:50 2022
+
+@author: aaboutal
+"""
+import numpy as np
+import astropy.units as u
+from astropy.constants.si import k_B, mu0
+
+def Alfven_sped(B: u.T, density: (u.kg / u.m**3)) -> u.m /u.s:
+    return np.abs(B)/ np.sqrt(mu0 * density)
