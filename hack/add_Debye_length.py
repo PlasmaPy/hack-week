0e0f077f2dba97bc264a620494d0e59a1e47827c
Xuan Sun <xuan.sun1@gmail.com>
2022-07-13 12:40:51 -0700

    Copied relevant code from PlasmaPy repo
    Took code snippets from PlasmaPy repo on GitHub and created a function for calculated Debye length.

--- hack/add_Debye_length.py
+++ hack/add_Debye_length.py
@@ -4,3 +4,11 @@ Created on Wed Jul 13 12:36:48 2022
 
 @author: xuans
 """
+import astropy.units as u
+import numpy as np
+
+from astropy.constants.si import c, e, eps0, k_B
+
+def Debye_length(T_e: u.K, n_e: u.m**-3) -> u.m:
+    
+    return np.sqrt(eps0 * k_B * T_e / (n_e * e**2))
