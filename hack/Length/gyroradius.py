87f625a487da7631b769630660547ce50ad320ce
Yi Zhou <Ghzhouyi@gmail.com>
2022-07-14 11:56:30 -0700

    Add validate_quantities and particle_input

--- hack/Length/gyroradius.py
+++ hack/Length/gyroradius.py
@@ -1,11 +1,16 @@
 import astropy.units as u
 import numpy as np
 from astropy.constants.si import e
+from plasmapy.utils.decorators import validate_quantities
+from plasmapy.particles import particle_input
+from plasmapy.particles import Particle
 
+@validate_quantities
+@particle_input
 def gyroradius(
-    mass: u.kg,
+    particle: Particle,
     B: u.T,
-    Vperp: u.m / u.s = np.nan * u.m / u.s,
+    Vperp: u.m / u.s,
 ) -> u.m:
     '''calculate gyro radius'''
-    return mass*Vperp/(B*e)
+    return particle.mass*np.abs(Vperp)/(B*particle.charge)
