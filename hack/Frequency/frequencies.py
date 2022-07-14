a826538c00d746b70389399f5338798e6f008344
Carlos A. Cartagena-Sanchez <ccartagena@brynmawr.edu>
2022-07-14 13:37:22 -0500

    Adding a new frequency function: Cyclotron Frequency

--- hack/Frequency/frequencies.py
+++ hack/Frequency/frequencies.py
@@ -2,12 +2,20 @@ import numpy as np
 import astropy.units as u
 from astropy.constants.si import eps0
 
-# Frequency of Diocotron modes associated with non-neutral single species plasmas
+
+def Cylotron_frequency(B: u.T, mass: u.kg, charge: u.C) -> u.rad/u.s:
+    """
+        Cylotron frequency
+    """
+    return charge*B/mass
 
 
 def Approx_diocotron_frequency(B: u.T, density: (u.kg/u.m**3), mass: u.kg, charge: u.C) -> u.rad/u.s:
-    # Best thing to do is to use existing functions (plasma frequency, cyclotron frequency) but
-    # since this is a demo I'm just going to write out the math
-    w_pe_sq = density*charge/(epsn0*mass**2)
-    w_ce = charge*B/mass
+    """ 
+        Frequency of Diocotron modes associated with non-neutral single species plasmas
+        Best thing to do is to use existing functions (plasma frequency, cyclotron frequency) but
+        since this is a demo I'm just going to write out the mat
+    """
+    w_pe_sq = density*charge/(eps0*mass**2)
+    w_ce = Cylotron_frequency(B, mass, charge)
     return np.pi*w_pe_sq/w_ce
